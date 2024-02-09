796963496961ada9ebe28fe9d8bebbe040fbf86a
Jake Leahy <jake@leahy.dev>
2024-02-09 13:56:04 +1100

    Very basic rendering working
    Will try and build a shitty simple DSL to make playing around easier
    
    Very basic rendering working
    
    Will try and build a shitty simple DSL to make playing around easier

--- signal.nim
+++ signal.nim
@@ -1,10 +1,17 @@
 when defined(js):
   import jsset
+  import std/dom
 else:
   import std/sets
 import macros
 import std/hashes
 
+type
+  NativeSet[T] = (when defined(js): JSSet[T] else: HashSet[T])
+
+proc initNativeSet[T](): NativeSet[T] =
+  when defined(js): newJSSet[T]()
+  else: initHashSet[T]()
 
 type
   Accessor[T] = proc (): T
@@ -17,6 +24,8 @@ type
     parent: Observer
     cleanups: seq[Callback]
     listeners: seq[Callback]
+    children: NativeSet[Observer]
+      ## Children observers so that we can call dispose and clear everything
 
   Computation = ref object of Observer
     fn: Callback
@@ -24,22 +33,29 @@ type
 proc hash(x: Observer): Hash =
   result = hash(cast[int](x.addr))
 
-var observer: Observer = Observer()
+var observer: Observer = Observer(children: initNativeSet[Observer]())
 
 proc initRoot(body: Callback) =
   let prev = observer
   body()
   observer = prev
 
+proc dispose(body: Observer) =
+  for child in body.children:
+    dispose child
+
+  for cleanup in body.cleanups:
+    cleanup()
+
 proc initComputation(body: Callback) =
   let prev = observer
-  observer = Computation(fn: body)
+  observer = Computation(fn: body, children: initNativeSet[Observer]())
+  prev.children.incl observer
   body()
   observer = prev
 
 proc run(x: Computation) =
-  for cleanup in x.cleanups:
-    cleanup()
+  x.dispose()
   x.fn()
 
 template staticSignal[T](val: T): Accessor[T] =
@@ -47,7 +63,7 @@ template staticSignal[T](val: T): Accessor[T] =
   fakeGet
 
 proc createSignal[T](init: T): Signal[T] =
-  var subscribers = when defined(js): newJSSet[Computation]() else: initHashSet[Computation]()
+  var subscribers = initNativeSet[Computation]()
 
   var value = init
   let read = proc (): T =
@@ -60,7 +76,8 @@ proc createSignal[T](init: T): Signal[T] =
   let write = proc (newVal: T) =
     value = newVal
     # Run every context that is subscribed
-    for subscriber in subscribers:
+    let old = subscribers
+    for subscriber in old:
       subscriber.run()
 
   return (read, write)
@@ -77,36 +94,58 @@ proc createMemo[T](callback: Accessor[T]): Accessor[T] =
 proc onCleanup(x: Callback) =
   observer.cleanups &= x
 
-# import std/dom
+when defined(js):
+
+  proc insert(parent: Element, child: Element, current: Element = nil) =
+    if current == nil:
+      parent.appendChild(child)
+    else:
+      parent.replaceChild(child, parent.firstChild)
+
+  proc insert(parent: Element, child: Accessor[Element], current: Element = nil) =
+    let current = child()
+    createEffect do ():
+      parent.insert(child(), current)
+
+  proc registerEvent(x: EventTarget, name: cstring, handler: proc (ev: Event)) =
+    x.addEventListener(name, handler)
+    onCleanup do ():
+      echo "Unregistering"
+      x.removeEventListener(name, handler)
+
+  proc counter(): Element =
+    let (count, setCount) = createSignal(0)
+
+    let btn = document.createElement("button")
+
+    btn.registerEvent("click") do (ev: Event):
+      setCount(count() + 1)
+
+    createEffect do ():
+      btn.innerText = cstring($count())
+
+    return btn
+
+  proc app(): auto =
+    let (showCounter, setShowCounter) = createSignal(true)
+
+    proc handleClick(ev: Event) =
+      echo "Clicked"
+      setShowCounter(not showCounter())
+
+    return createMemo do () -> Element:
+      if showCounter():
+        let btn = document.createElement("button")
+        btn.innerText = "Hello"
+        btn.registerEvent("click", handleClick)
+        return btn
+      else:
+        let btn = document.createElement("button")
+        btn.innerText = "Goodbye"
+        btn.registerEvent("click", handleClick)
+        return btn
+
+  document.body.insert(app())
 
 
-# proc counter(): Element =
-#   let (count, setCount) = createSignal(0)
-#
-#   let btn = document.createElement("button")
-#
-#   btn.onclick = proc (e: Event) =
-#     setCount(count() + 1)
-#
-#   createEffect do ():
-#     btn.innerText = cstring($count())
-#
-#   return btn
-#
-# document.body.appendChild(counter())
-
-
-let (count, setCount) = createSignal(0)
-
-let countSquared = createMemo do () -> int:
-  echo "Recomputing"
-  onCleanup do ():
-    echo "Cleaning up"
-  count() * count()
-
-echo countSquared()
-setCount(9)
-echo countSquared()
-echo countSquared()
-echo countSquared()
 
