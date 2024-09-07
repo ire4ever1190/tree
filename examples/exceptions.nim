2af098152342aeda4d321d9878fc2da13a4eb3cb
Jake Leahy <jake@leahy.dev>
2024-09-07 16:09:48 +1000

    Start on examples

---
+++ examples/exceptions.nim
@@ -0,0 +1,47 @@
+##[
+  This is an example on how exceptions work.
+
+  Due to how exception handling works in Nim, this only catches exceptions that are thrown during the creation of
+  a widget
+]##
+
+import fogair
+import std/dom
+
+const defaultCount = 5
+
+proc Counter(x: Accessor[int]): Element =
+  # If this was wrapped in a createEffect, then it would just be treated
+  # as an unhandled exception
+  if x() == 0:
+    raise (ref CatchableError)(msg: "I hate being 0 >:(")
+
+  gui:
+    text("Everything is fine (As long as I don't become 0)")
+
+proc App(): Element =
+  let (count, setCount) = createSignal(defaultCount)
+  # Start counting down
+  var interval: Interval
+  interval = setInterval(proc () =
+    if count() == 0: return
+    setCount(count() - 1)
+  , 1000)
+  # Build the GUI
+  gui:
+    tdiv:
+      p:
+        text(proc (): string = "The count is " & $count())
+      tdiv:
+        try:
+          Counter(count)
+        except CatchableError as e:
+          tdiv:
+            p:
+              text(e.msg)
+            button:
+              proc click() = setCount(defaultCount)
+              text("Click me to fix it")
+
+
+discard document.getElementById("root").insert(App)
