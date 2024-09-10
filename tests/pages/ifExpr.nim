a93e7329d1676221cd29b264e7b870ff54b34626
Jake Leahy <jake@leahy.dev>
2024-09-10 20:35:43 +1000

    Forgot to add if expression test

---
+++ tests/pages/ifExpr.nim
@@ -0,0 +1,42 @@
+when defined(buildPage):
+  import fogair
+  import std/dom
+
+  proc App(): Element =
+    let (flag, setFlag) = createSignal(false)
+    gui:
+      tdiv:
+        button(id="btnFlip"):
+          proc click = setFlag(not flag())
+        # Test elements can be conditional
+        if flag():
+          p(id="toggled"):
+            text "I'm shown"
+        # Test that elements get cleaned
+        if flag():
+          p(id="whenTrue"):
+            text("Its true")
+        else:
+          p(id="whenFalse"):
+            text("Its false")
+
+  discard document.getElementById("root").insert(App)
+
+else:
+  import utils
+
+  proc tests*(d: FirefoxDriver) {.async.} =
+    template testForState(state: bool) =
+      test "Flipped element is correct":
+        check d.elementExists("#toggled").await() == state
+
+      test "The " & $state & " element is shown":
+        check d.elementExists("#whenFalse").await() != state
+        check d.elementExists("#whenTrue").await() == state
+
+    testForState(false)
+    await d.selectorClick("#btnFlip")
+    testForState(true)
+    # Check they can handle cleaning up after
+    await d.selectorClick("#btnFlip")
+    testForState(false)
